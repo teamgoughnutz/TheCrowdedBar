8fb28d83e2a21e635a767b2c4ca111c4f2cf549d
Dylan Davidson <rogue1037@gmail.com>
2019-04-25 14:13:51 -0700

    adding non drink model

---
+++ models/nondrink.js
@@ -0,0 +1,26 @@
+module.exports = function (sequelize, DataTypes) {
+    var Nondrink = sequelize.define("Nondrink", {
+      title: {
+        type: DataTypes.STRING(140),
+        allowNull: false,
+        validate: {
+          len: [1,140]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT(400),
+        allowNull: false,
+        len: [1]
+      },
+      catergory: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "Stay Sober"
+      }
+  
+    });
+  
+  
+    return Nondrink;
+  
+  };
