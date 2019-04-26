8bb6cb217080188175e6736a7c6814a91feb08e7
Dylan Davidson <rogue1037@gmail.com>
2019-04-26 11:12:21 -0700

    fixed the database

--- models/drink.js
+++ models/drink.js
@@ -1,26 +1,9 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var Drink = sequelize.define("Drink", {
-    title: {
-      type: DataTypes.STRING(140),
-      allowNull: false,
-      validate: {
-        len: [1,140]
-      }
-    },
-    body: {
-      type: DataTypes.TEXT(400),
-      allowNull: false,
-      len: [1]
-    },
-    catergory: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "Get Drunk"
-    }
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    catergory: DataTypes.STRING
 
   });
-
-
   return Drink;
-
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,26 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/drinks", function(req, res) {
+    db.Drink.findAll({}).then(function(drinks) {
+      res.json(drinks);
     });
+    console.log(drinks);
   });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  
 };
+
+//   // Create a new example
+//   app.post("/api/examples", function(req, res) {
+//     db.Drink.create(req.body).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+
+//   // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Drink.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
