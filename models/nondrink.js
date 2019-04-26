92e2b3dadb47c02606495323bd81e78f9180466e
Dylan Davidson <rogue1037@gmail.com>
2019-04-26 10:01:25 -0700

    added to drinks.js

--- models/nondrink.js
+++ models/nondrink.js
@@ -19,8 +19,8 @@ module.exports = function (sequelize, DataTypes) {
       }
   
     });
-  
+    
   
     return Nondrink;
-  
+    
   };
--- public/js/drinks.js
+++ public/js/drinks.js
@@ -23,36 +23,36 @@ $.get("/api/drinks/all", function(data) {
 });
 
 // When user chirps (clicks addBtn)
-$("#drink-submit").on("click", function(event) {
-  event.preventDefault();
+// $("#drink-submit").on("click", function(event) {
+//   event.preventDefault();
 
-  // Make a newChirp object
-  var newDrink = {
-    title: $("#title").val().trim(),
-    body: $("#drinks-box").val().trim(),
-    catergory: $("#drinks-box").val().trim(),
-  };
+//   // Make a newChirp object
+//   var newDrink = {
+//     title: $("#title").val().trim(),
+//     body: $("#drinks-box").val().trim(),
+//     catergory: $("#drinks-box").val().trim(),
+//   };
 
-  console.log(newDrink);
+//   console.log(newDrink);
 
-  // Send an AJAX POST-request with jQuery
-  $.post("/api/drink", newDrink)
-    // On success, run the following code
-    .then(function() {
+//   // Send an AJAX POST-request with jQuery
+//   $.post("/api/drink", newDrink)
+//     // On success, run the following code
+//     .then(function() {
 
-      var row = $("<div>");
-      row.addClass("drink");
+//       var row = $("<div>");
+//       row.addClass("drink");
 
-      row.append("<p>" + newDrink.title + "</p>");
-      row.append("<p>" + newDrink.body + "</p>");
-      row.append("<p>" + newDrink.catergory + "</p>");
+//       row.append("<p>" + newDrink.title + "</p>");
+//       row.append("<p>" + newDrink.body + "</p>");
+//       row.append("<p>" + newDrink.catergory + "</p>");
      
 
-      $("#drinks-area").prepend(row);
+//       $("#drinks-area").prepend(row);
 
-    });
+//     });
 
-  // Empty each input box by replacing the value with an empty string
-  $("#title").val("");
-  $("#drinks-box").val("");
-});
+//   // Empty each input box by replacing the value with an empty string
+//   $("#title").val("");
+//   $("#drinks-box").val("");
+// });
