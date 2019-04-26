cf20c063207b905e50cf07acc0c390d67b9c1b00
Cris <cherrera22@gmail.com>
2019-04-26 11:53:22 -0700

    adding routes models and html for test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,49 @@
+//The Crowded Bar API Routes
+
+//Dependencies
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+//Routes
+module.exports = function (app) {
+  //GET route for all drinks
+  app.get("/api/drinks", function (req, res) {
+    //sequelize code to find all drinks and return them to the user with res.json
+    db.Drink.findAll({}).then(result => res.json(result))
+      .catch(function (err) {
+        console.log(err.message);
+        res.send(500);
+      });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //GET route for returning all drinks of a specific category (either GETDRUNK or STAYSOBER)
+  app.get("/api/drinks/category/:category", function (req, res) {
+    // Add sequelize code to find all drinks where the category is equal to req.params.category,
+    db.Drink.findAll({
+      where: {
+        category: req.params.category
+      }
+      // return the result to the user with res.json
+    }).then(function (result) {
+      res.json(result)
+    })
+      .catch(err => {
+        console.log(err.message);
+        res.send(500);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //GET route for retrieving a single drink
+  app.get("/api/drinks/:id", function (req, res) {
+    // Add sequelize code to find a single drink where the id is equal to req.params.id,
+    db.Drink.findOne({
+      where: {
+        id: req.params.id
+      }
+      // return the result to the user with res.json
+    }).then(result => res.json(result))
+      .catch(function (err) {
+        console.log(err.message);
+        res.send(500);
+      });
   });
 };
