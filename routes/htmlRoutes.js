f414e926b46cbcba5e270f26fec3cad2aa15e23f
Jake Hart <caboky31@gmail.com>
2019-04-30 10:57:54 -0700

    changed comment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,6 @@
 var db = require("../models");
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
