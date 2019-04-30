fc3d48d701bcf2bbd59738a6454f9797aa566708
Dylan Davidson <rogue1037@gmail.com>
2019-04-30 10:18:37 -0700

    adding Nondrink to api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -153,6 +153,81 @@ module.exports = function(app) {
       });
   });
 
+  app.get("/api/posts", function (req, res) {
+    db.Nondrink.findAll({}).then(result => res.json(result))
+      .catch(function (err) {
+        console.log(err.message);
+        res.send(500);
+      });
+  });
+
+  app.get("/api/posts/category/:category", function (req, res) {
+    db.Nondrink.findAll({
+      where: {
+        category: req.params.category
+      }
+    }).then(function (result) {
+      res.json(result)
+    })
+      .catch(err => {
+        console.log(err.message);
+        res.send(500);
+      });
+  });
+
+  app.get("/api/posts/:id", function (req, res) {
+    db.Nondrink.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(result => res.json(result))
+      .catch(function (err) {
+        console.log(err.message);
+        res.send(500);
+      });
+  });
+
+  app.post("/api/posts", function (req, res) {
+    db.Nondrink.create({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    }).then(result => res.json(result))
+      .catch(function (err) {
+        console.log(err.message);
+        res.send(500);
+      });
+  });
+
+  app.delete("/api/posts/:id", function (req, res) {
+    db.Nondrink.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(result => res.json(result))
+      .catch(function (err) {
+        console.log(err.message);
+        res.send(500);
+      });
+  });
+
+  app.put("/api/posts", function (req, res) {
+    db.Nondrink.update({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    }, {
+        where: {
+          id: req.body.id
+        }
+      }).then(result => res.json(result))
+      .catch(function (err) {
+        console.log(err.message);
+        res.send(500);
+      });
+  });
+  
+
    
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
