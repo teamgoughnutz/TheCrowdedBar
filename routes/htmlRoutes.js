c1a656cb7d9afd4b1969f4daa9765e412aeb52fe
Cris <cherrera22@gmail.com>
2019-05-01 12:01:39 -0700

    fix signup route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -21,7 +21,7 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../private/virgin.html"));
   });
 
-  app.get("/login", function (req, res) {
+  app.get("/signup", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
